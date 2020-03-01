f9e83823564dd64729ad465587dd1be1b9f674d9
Dhea Melyadinata <dmelyadinata@gmail.com>
2020-03-01 10:02:01 +0700

    Membuat tampilan splashscreen dan java

---
+++ app/src/main/java/com/example/aplikasiportofolioanak/Splashscreen.java
@@ -0,0 +1,34 @@
+package com.example.aplikasiportofolioanak;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                        sleep(3000); //3detik
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
